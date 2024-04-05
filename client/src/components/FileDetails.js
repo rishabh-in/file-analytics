5ca0f1a9949a291802f01d17c0dea124cbf67258
Rishabh Tiwari <13rishabhtiwari@gmail.com>
2024-04-05 17:44:17 +0530

    file details page added

--- client/src/components/FileDetails.js
+++ client/src/components/FileDetails.js
@@ -1,11 +1,59 @@
 import React from 'react'
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom'
+import Loading from './common/Loading';
+import { Table, Tag } from 'antd';
 
 const FileDetails = () => {
   const {id} = useParams();
-  console.log(id);
+  
+  const fetchWordCountDetailsOfFile = async() => {
+    const response = await fetch(`http://localhost:4001/api/files/${id}/words`);
+    const json = await response.json();
+    return json
+  }
+
+  // fetch data using id
+  const {data, isLoading, error} = useQuery("word", fetchWordCountDetailsOfFile);
+  if(isLoading) return <Loading />;
+  if(error) return <div>Something web wrong</div>
+  const columns = [
+    {
+      title: "Word",
+      dataIndex: "word",
+      key: "word"
+    },
+    {
+      title: "Totle Count",
+      dataIndex: "totalCount",
+      key: "totalCount"
+    },
+    {
+      title: "Synonyms",
+      dataIndex: "synonyms",
+      key: "synonyms",
+      render: (syn) => (
+        <>
+          {syn && syn.length > 0 ? syn.map((s) => (
+            <Tag color="geekblue">{s?.text}</Tag>
+          )): <Tag>No Synonyms Found</Tag>}
+        </>
+      )
+    }
+  ]
+
+  const dataSet = data.data.map((d) => {
+    const limitedSynonyms = d.synonyms.slice(0, 5);
+    return {
+      word: d.word,
+      totalCount: d.totalCount,
+      synonyms: limitedSynonyms
+    }
+  })
   return (
-    <div>FileDetails</div>
+    <div className='w-full flex justify-center mt-5'>
+      <Table className='w-7/12 rounded-xl shadow-xl' columns={columns} dataSource={dataSet} />
+    </div>
   )
 }
 
