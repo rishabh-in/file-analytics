f95e2977483f9d58cdb70a18addad3f48a90e07b
Rishabh Tiwari <13rishabhtiwari@gmail.com>
2024-04-18 01:15:26 +0530

    socket origin port chnaged

--- server/utils/setupSockets.js
+++ server/utils/setupSockets.js
@@ -2,7 +2,7 @@ import {Server} from 'socket.io';
 
 const setupSockets = (server) => {
   const soc = new Server(server, {
-    cors: {origin:"http://localhost:3000", methods: ["GET", "POST"]},
+    cors: {origin:"http://localhost:4000", methods: ["GET", "POST"]},
   });
 
   soc.on("connect", (socket) => {
